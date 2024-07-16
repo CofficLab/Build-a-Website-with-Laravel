0e644603c2ac385735f405b942221b29d1d7ffff
Nookery <nooks@qq.com>
2024-07-16 10:59:00 +0800

    集成tailwindcss

--- store-app/resources/views/welcome.blade.php
+++ store-app/resources/views/welcome.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        @vite('resources/css/app.css')
-    </head>
-    <body class="">
-        <h1>hello</h1>
-    </body>
+    <title>Laravel</title>
+
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-red-200">
+    <h1 class="text-3xl">hello</h1>
+</body>
+
 </html>
